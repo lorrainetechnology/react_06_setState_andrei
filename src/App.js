4b5590a5be06737f50140059e9ad6b7452a07e16
lorrainetechnology <lorrainetechnology@gmail.com>
2022-05-10 18:19:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: 'Yihua', lastName: 'Zhang' },
+      company: 'ZTM'
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi { this.state.name.firstName } { this.state.name.lastName }, I work at {this.state.company}</p>
+          <button onClick={() => {
+            this.setState(() => {
+              return {name: {firstName: 'Andrei', lastName: 'Neagoie'}};
+            });
+          }}>Change Name</button>
+        </header>
+      </div>
+    );
+  }  
+}
+
+export default App;
